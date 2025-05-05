1672c94b0326f443d93ec5fc6f7379016f5f8b93
Rotem Dan <rotemdan@gmail.com>
2025-05-05 12:16:09 +0300

    Update test

--- src/Test.ts
+++ src/Test.ts
@@ -1,13 +1,13 @@
 import { decodeWaveToFloat32Channels, encodeWaveFromFloat32Channels, SampleFormat } from './WaveCodec.js'
 
 function test() {
-	const channel1 = new Float32Array([0.1, 0.2, 0.3])
-	const channel2 = new Float32Array([0.2, 0.3, 0.4])
-	const channel3 = new Float32Array([0.3, 0.4, 0.5])
+	const channel1 = new Float32Array([-0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3])
+	const channel2 = new Float32Array([-0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4])
+	const channel3 = new Float32Array([-0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
 
 	const inputChannels = [channel1, channel2, channel3]
 
-	const encoded = encodeWaveFromFloat32Channels(inputChannels, 1234, 16, SampleFormat.PCM)
+	const encoded = encodeWaveFromFloat32Channels(inputChannels, 1234, 24, SampleFormat.PCM)
 	const decoded = decodeWaveToFloat32Channels(encoded)
 
 	console.log(inputChannels)
